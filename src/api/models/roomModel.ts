7e19cd79f3f390004eca9ee291fb9a1f804f57a6
beauspot <42305168+beauspot@users.noreply.github.com>
2026-02-28 05:11:42 +0100

    (Chore): created this model to handle the rooms data

---
+++ src/api/models/roomModel.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const RoomSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    maxPeople: {
+      type: Number,
+      required: true,
+    },
+    desc: {
+      type: String,
+      requireed: true,
+    },
+    roomNumbers: [
+      {
+        number: Number,
+        unavailableDates: { type: [Date] },
+      },
+    ],
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("Rooms", RoomSchema);
